6244c69d73867477e05266dfa6eeae982e3fd069
FelipeSC97 <felipesanchezc97@gmail.com>
2024-12-05 21:24:06 -0500

    Cambios menores a dataframe final

--- Results/Concat_Results.py
+++ Results/Concat_Results.py
@@ -6,13 +6,23 @@ folder_path = r"C:\Users\fsanc\OneDrive - Universidad EAFIT\Documentos\Pipe\OneD
 dataframes = []
 
 for file in os.listdir(folder_path):
-    if file.endswith(".csv"):  # Verificar que el archivo sea un CSV
+    if file.endswith(".csv"): 
         file_path = os.path.join(folder_path, file)
-        df = pd.read_csv(file_path)  # Cargar el archivo CSV
+        df = pd.read_csv(file_path)  # Cargar sólo archivos CSV
         dataframes.append(df)
 
 concatenated_df = pd.concat(dataframes, ignore_index=True)
+concatenated_df['model_type'] = concatenated_df['model_name'].apply(
+    lambda x: "Machine Learning" if x in ["RandomForestRegressor", "XGBRegressor"] else "Estadístico Tradicional"
+)
+concatenated_df = concatenated_df.sort_values(by='model_type', key=lambda x: x.map({
+    "Estadístico Tradicional": 0,
+    "Machine Learning": 1
+}))
+columns_order = ['model_type'] + [col for col in concatenated_df.columns if col != 'model_type']
+concatenated_df = concatenated_df[columns_order]
 
+# Generar nuevo archivo
 output_path = os.path.join(folder_path, "Resultados.xlsx")
 concatenated_df.to_excel(output_path, index=False, header=True)
 
